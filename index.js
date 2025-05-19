73e433b130b215a26534784aa2e94e1d33be4182
Rakesh Shriwas <rakesh.shriwas@nagarro.com>
2025-05-19 16:12:16 +0530

    clear db

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
+// const jsonServer = require("json-server"); // importing json-server library
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
-server.listen(port);
+// server.listen(port);
+
+//---------------------------------------------------------------------------------------
+
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
+
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+// Bind the router db to the app
+app.db = router.db;
+
+app.use(cors());
+app.use(jsonServer.defaults());
+app.use(auth); // <== Add auth middleware before router
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('JSON Server is running on http://localhost:3000');
+});
